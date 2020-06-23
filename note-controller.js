2f00203b164a5bbc3cc72d92b851e6ceabeb179c
Nikita <latinisnotdead07@gmail.com>
2020-06-23 18:02:19 +0100

    NoteController renders html

--- note-controller.js
+++ note-controller.js
@@ -5,26 +5,19 @@
     this.view = new NoteListView(this.notelist);
   }
 
-  // NoteController.prototype.addNoteToList = function () {
-  //   return 
-  // };
-
-  // NoteController.prototype.createView = function () {
-  //   var view = new NoteListView(this.notelist);
-  // };
-
   NoteController.prototype.renderHtml = function () {
     var test = document.getElementById('app');
     test.innerHTML = this.view.htmlWrap();
   };
 
-  document.addEventListener('DOMContentLoaded', function (event) {
-  var list = new NoteList();
-  var test = new NoteController(list);
-  test.renderHtml();
-});
-
   exports.NoteController = NoteController;
 
-});(this);
+})(this);
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  var list = new NoteList
+  var controller = new NoteController(list)
+  controller.renderHtml();
+  });
+
 
