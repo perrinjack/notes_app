21739f86d99f812fa9a28b673045d900f869e598
Nikita <latinisnotdead07@gmail.com>
2020-06-23 15:58:53 +0100

    inserts HTML into index.html

---
+++ note-controller.js
@@ -0,0 +1,6 @@
+console.log(document.getElementById('app'));
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var test = document.getElementById("app").textContent();
+  console.log(test);
+})
