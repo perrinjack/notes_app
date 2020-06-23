5ddb6acbbb1c2eba7fcb1f9cda03623deb4d08cd
J Perrin <perrinjack96@gmail.com>
2020-06-23 17:12:19 +0100

    change roles

--- note-controller.js
+++ note-controller.js
@@ -3,28 +3,22 @@
     this.notelist = NoteList;
   }
 
-  NoteController.prototype.addNoteToList= function () {
+  NoteController.prototype.addNoteToList = function () {
     return this.notelist.addNote('Beer');
   };
 
-NoteController.prototype.createView = function () {
-var view = new NoteListView(this.notelist)
-
-}
-NoteController.prototype.renderHtml = function () {
-
-  var test = document.getElementById('app');
-  test.innerHTML = view ;
-}
-
-
-
+  NoteController.prototype.createView = function () {
+    var view = new NoteListView(this.notelist);
+  };
+  NoteController.prototype.renderHtml = function () {
+    var test = document.getElementById('app');
+    test.innerHTML = view;
+  };
 
   exports.NoteController = NoteController;
 });
 this;
 
 document.addEventListener('DOMContentLoaded', function (event) {
-  
   console.log(test);
 });
